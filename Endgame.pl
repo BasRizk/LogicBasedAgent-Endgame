493b1a5f2290cce4471472c6aee8c700aff811ee
mike13k <mikegeorge2012@gmail.com>
2019-11-18 13:37:21 +0200

    fix some mistakes

--- Endgame.pl
+++ Endgame.pl
@@ -1,10 +1,6 @@
-gridSize(5,5).
-iAt(1,2,s0).
-tAt(3,4).
-sAt(1,1,s0).
-sAt(2,1,s0).
-sAt(2,2,s0).
-sAt(3,3,s0).
+gridSize(2,2).
+iAt(0,0,s0).
+tAt(1,1).
 
 
 action(up).
@@ -23,7 +19,7 @@ result(A,S):-
 
 iAt(X,Y,SN):-
     iAt(X,Y,SO),
-    sAt(X,Y,SO)
+    sAt(X,Y,SO),
     SN = result(A,SO),
     A = collect.
 
