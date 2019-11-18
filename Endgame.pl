a0246424850db668511fea0bdd43447c785f67fa
mike13k <mikegeorge2012@gmail.com>
2019-11-18 12:05:52 +0200

    edit SSA to fix mistakes

--- Endgame.pl
+++ Endgame.pl
@@ -1,3 +1,12 @@
+gridSize(5,5).
+iAt(1,2,s0).
+tAt(3,4).
+sAt(1,1,s0).
+sAt(2,1,s0).
+sAt(2,2,s0).
+sAt(3,3,s0).
+
+
 action(up).
 action(down).
 action(left).
@@ -15,7 +24,7 @@ result(A,S):-
 iAt(X,Y,SN):-
     iAt(X,Y,SO),
     SN = result(A,SO),
-    A \= action(left), A \= action(up), A \= action(right), A \= action(down).
+    A = collect.
 
 iAt(X,YN,SN):-
     iAt(X,YO,SO),
@@ -25,28 +34,38 @@ iAt(X,YN,SN):-
     A = left.
 iAt(X,YN,SN):-
     iAt(X,YO,SO),
-    YO is YN - 1,
+    YO is YN + 1,
+    action(A),
+    SN = result(A,SO),
+    A = right.
+iAt(XN,Y,SN):-
+    iAt(XO,Y,SO),
+    XO is XN + 1,
     action(A),
     SN = result(A,SO),
     A = down.
-iAt(X,YN,SN):-
-    iAt(X,YO,SO),
-    YO is YN - 1,
+iAt(XN,Y,SN):-
+    iAt(XO,Y,SO),
+    XO is XN - 1,
     action(A),
     SN = result(A,SO),
-    A = down.
-iAt(X,YN,SN):-
-    iAt(X,YO,SO),
-    YO is YN - 1,
-    action(A),
-    SN = result(A,SO),
-    A = down.
+    A = up.
 
 
 sAt(X,Y,SN):-
     SN = result(A,SO),
+    sAt(X,Y,s0),
     A \= action(collect).
 
 
+snapped1(S):-
+    S = result(snap, SO),
+    tAt(X,Y),
+    iAt(X,Y,SO).
+
+
 snapped(S):-
-    S = result(snap, SO).
+    gener(L),
+    call_with_depth_limit(snapped1(S),L,R),
+    R \= depth_limit_exceeded,
+    !.
