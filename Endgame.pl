40d82aed619c76ea2059bc80f9f0f8b524453057
mike13k <mikegeorge2012@gmail.com>
2019-11-20 20:52:25 +0200

    add modifications that allow the snapped query, when called with a correct situation, to return true, and false otherwise

--- Endgame.pl
+++ Endgame.pl
@@ -1,11 +1,9 @@
 % Example at the point
-gridSize(5,5).
-iAt(1,2,s0).
-tAt(3,4).
-sAt(1,1,s0).
-sAt(2,1,s0).
-sAt(2,2,s0).
-sAt(3,3,s0).
+gridSize(2,2).
+iAt(0,0,s0).
+tAt(1,1).
+sAt(1,0,0,0,s0).
+sAt(2,1,0,0,s0).
 
 %       0   1   2   3   4
 %   0 |   |   |   |   |   |
@@ -34,77 +32,99 @@ result(A,S):-
     action(A),
     S = result(_,_).
 
+
 % Stone At (X, Y, S2) exists there if it was already there and was not collected before.
-sAt(X,Y,SN):-
-    SN = result(A,S0),
-    sAt(X,Y,S0),
+% sAt(ID,X,Y,0,SN):-
+%     SN = result(A,S0),
+%     sAt(ID,X,Y,0,S0),
+%     action(A),
+%     A \= collect.
+    
+sAt(ID,X,Y,1,SN):-
+    SN = result(A,_),
     action(A),
-    A \= collect.
+    A = collect,
+    sAt(ID,X,Y,0,s0).
+
+% sAt(ID,X,Y,1,SN):-
+%     SN = result(A,S0),
+%     sAt(ID,X,Y,1,S0),
+%     action(A),
+%     A \= collect.
 
 % Ironman At (X, Y, SN) exists there if he was already there, and there was stone there, and he collected it.
 iAt(X,Y,SN):-
+    SN \= s0,
     SN = result(A,S0),
-    iAt(X,Y,S0),
-    sAt(X,Y,S0),
-    A = collect.
+    A = collect,
+    sAt(ID,X,Y,0,s0),
+    sAt(ID,X,Y,1,SN),
+    iAt(X,Y,S0).
 
 % Ironman at (X, Y, SN) exists there if he was on Y0 before which is at least one cell away from the borders (0-index),
 % and he moved left.
 iAt(X,YN,SN):-
+    SN \= s0,
     SN = result(A,S0),
+    action(A),
+    A = left,
     iAt(X,Y0,S0),
     Y0 > 0,
-    YN is Y0 - 1,
-    action(A),
-    % iAt(X, YN, SN),
-    A = left.
+    YN is Y0 - 1.
+
 
 % Ironman at (X, Y, SN) exists there if he was on Y0 before which is at least one cell away from the borders (grid size),
 % and he moved right.
 iAt(X,YN,SN):-
+    SN \= s0,
     SN = result(A,S0),
+    action(A),
+    A = right,
     iAt(X,Y0,S0),
     gridSize(_,GY),
     Y0 < GY - 1,
-    YN is Y0 + 1,
-    action(A),
-    A = right.
+    YN is Y0 + 1.
+
 
 % Ironman at (X, Y, SN) exists there if he was on X0 before which is at least one cell away from the borders (grid size),
 % and he moved down.
 iAt(XN,Y,SN):-
+    SN \= s0,
     SN = result(A,S0),
+    action(A),
+    A = down,
     iAt(X0,Y,S0),
     gridSize(GX,_),
     X0 < GX - 1,
-    XN is X0 + 1,
-    action(A),
-    A = down.
+    XN is X0 + 1.
+
 
 % Ironman at (X, Y, SN) exists there if he was on X0 before which is at least one cell away from the borders (0-index),
 % and he moved up.
 iAt(XN,Y,SN):-
+    SN \= s0,
     SN = result(A,S0),
+    action(A),
+    A = up,
     iAt(X0,Y,S0),
     X0 > 0,
-    XN is X0 - 1,
-    action(A),
-    A = up.
+    XN is X0 - 1.
+
 
 % Snap is performed if there exists a situation S0 previously where thanos and ironman at the same position.
 % TODO check stones were collected!!
 snapped1(S):-
     S = result(snap, S0),
     tAt(X, Y),
+    sAt(1,_,_,1,_),
+    sAt(2,_,_,1,_),
     iAt(X, Y, S0).
 
 
+
 % Enforce an iterative deepening mechanism to enforce in turn completeness
-generateLimit(10).
-generateLimit(L):-
-    generateLimit(O),
-    O < 50,
-    L is O + 1.
+generateLimit(20).
+
 
 % snapped only if at some depth of a search tree there exists a snapped solution.
 snapped(S):-
