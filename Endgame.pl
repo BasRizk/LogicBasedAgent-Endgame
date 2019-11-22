5824dd4e47c3c79f18e3554b78c4e1bbdf546398
Basem Rizk <basem.rizk@outlook.com>
2019-11-22 14:47:17 +0200

    just some good refactoring

--- Endgame.pl
+++ Endgame.pl
@@ -1,13 +1,12 @@
-% Example at the point
-% gridSize(5,5).
-% iAt(1,2,s0).
-% tAt(3,4).
-% sAt(1,1,1,0,s0).
-% sAt(2,2,1,0,s0).
-% sAt(3,2,2,0,s0).
-% sAt(4,3,3,0,s0).
-
-
+% Example 1
+% 
+gridSize(5,5).
+tAt(3,4).
+sAt(1,1,1,s0).
+sAt(2,2,1,s0).
+sAt(3,2,2,s0).
+sAt(4,3,3,s0).
+iAt(1,2,s0).
 
 %       0   1   2   3   4
 %   0 |   |   |   |   |   |
@@ -17,16 +16,15 @@
 %   4 |   |   |   |   |   |
 
 
-
+% EXAMPLE 2
+% 
 % gridSize(5,5).
 % iAt(2,2,s0).
 % tAt(4,2).
-% sAt(1,4,0,0,s0).
-% sAt(2,1,2,0,s0).
-% sAt(3,3,0,0,s0).
-% sAt(4,2,1,0,s0).
-
-
+% sAt(1,4,0,s0).
+% sAt(2,1,2,s0).
+% sAt(3,3,0,s0).
+% sAt(4,2,1,s0).
 
 %       0   1   2   3   4
 %   0 |   |   |   |   |   |
@@ -35,85 +33,63 @@
 %   3 | s |   |   |   |   |
 %   4 | s |   | t |   |   |
 
-
-
+% Example 3 (Simple)
+% 
 % gridSize(2,2).
 % iAt(0,0,s0).
 % tAt(1,1).
-% sAt(1,0,0,0,s0).
-% sAt(2,1,0,0,s0).
-
-
-
-% In a state "SN", A stone was collected before in position (X,Y) if and only if
-% A "collect" action was performed in the previous state "S0" and Ironman was at (X,Y), where the stone exists and was not collected before.
-% Or, in "S0", the stone was collected before.
-sAt(ID,X,Y,1,SN):-
-    (SN = result(A,S0),
-    A = collect,
-    sAt(ID,X,Y,0,s0),
-    iAt(X,Y,S0));
-
-    (SN = result(A,S0),
-    sAt(ID,X,Y,1,S0)).
-
+% sAt(1,0,0,s0).
+% sAt(2,1,0,s0).
 
 % Ironman At (X, Y, SN) exists there if he was already there, and he performed the collect action.
-iAt(X,Y,SN):-
-    (SN = result(A,S0),
-    A = collect,
+iAt(X,Y,result(A, S0)):-
+    (A = collect,
     iAt(X,Y,S0));
 
-% Ironman at (X, Y, SN) exists there if he was on Y0 before which is at least one cell away from the borders (0-index),
-% and he moved left.
-    (SN = result(A,S0),
-    A = left,
+    (A = left,
     iAt(X,Y0,S0),
     Y0 > 0,
     Y is Y0 - 1);
 
-
-% Ironman at (X, Y, SN) exists there if he was on Y0 before which is at least one cell away from the borders (grid size),
-% and he moved right.
-    (SN = result(A,S0),
-    A = right,
+    (A = right,
     iAt(X,Y0,S0),
     gridSize(_,GY),
     Y0 < GY - 1,
     Y is Y0 + 1);
 
-
-% Ironman at (X, Y, SN) exists there if he was on X0 before which is at least one cell away from the borders (grid size),
-% and he moved down.
-    (SN = result(A,S0),
-    A = down,
+    (A = down,
     iAt(X0,Y,S0),
     gridSize(GX,_),
     X0 < GX - 1,
     X is X0 + 1);
 
-
-% Ironman at (X, Y, SN) exists there if he was on X0 before which is at least one cell away from the borders (0-index),
-% and he moved up.
-    (SN = result(A,S0),
-    A = up,
+    (A = up,
     iAt(X0,Y,S0),
     X0 > 0,
     X is X0 - 1).
 
+% In a state "SN", A stone was collected before in position (X,Y) if and only if
+% A "collect" action was performed in the previous state "S0" and Ironman was at (X,Y), where the stone exists and was not collected before.
+% Or, in "S0", the stone was collected before.
+stoneExists(ID, X, Y, 0, s0):-
+    sAt(ID, X, Y, s0).
+
+stoneExists(ID,X,Y,1,result(A,S0)):-
+    (A = collect,
+    stoneExists(ID,X,Y,0,s0),
+    iAt(X,Y,S0));
+
+    stoneExists(ID,X,Y,1,S0).
+
 
 % Snap is performed if there exists a situation S0 previously where thanos and ironman at the same position.
-% TODO check stones were collected!!
-snapped1(S):-
-    S = result(snap, S0),
+snapped1(result(snap, S0)):-
     tAt(X, Y),
     iAt(X, Y, S0),
-    sAt(1,_,_,1,S0),
-    sAt(2,_,_,1,S0),
-    sAt(3,_,_,1,S0),
-    sAt(4,_,_,1,S0).
-
-
+    stoneExists(1,_,_,1,S0),
+    stoneExists(2,_,_,1,S0),
+    stoneExists(3,_,_,1,S0),
+    stoneExists(4,_,_,1,S0).
 
 % Enforce an iterative deepening mechanism to enforce in turn completeness
 generateLimit(1).
